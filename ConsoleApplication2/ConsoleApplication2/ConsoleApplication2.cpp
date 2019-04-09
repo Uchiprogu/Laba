d6678439260945fcdc60d0bccd58a6428a8188c2
Uchiprogu <39963050+Uchiprogu@users.noreply.github.com>
2019-04-09 12:02:24 +0300

    END

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -19,20 +19,38 @@ template< class T >
 void input(vector<T> &arr,const int sizeKey = 0);
 
 template< class T, class B>
-void encryption(vector<T> &arr, vector<B> key);
+void encryption(vector<T> &arr, vector<B>& key);
 template< class T, class B>
 void decryption(vector<T> &arr, vector<B> key);
 
+struct Key
+{
+	char ch;
+	int number;
+};
+
 int main()
 {
-	vector<char> key;
+	Key keyCharacter;
+	vector<Key> key;
 	vector<string> myV;
-	key.push_back('A');
-	key.push_back('O');
-	key.push_back('A');
-	key.push_back('D'); 
-	key.push_back('C');
-	key.push_back('B');
+
+	key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+
+	key.at(0).number = 0;
+	key.at(0).ch = 'A';
+	key.at(1).number = 1;
+	key.at(1).ch = 'N';
+	key.at(2).number = 2;
+	key.at(2).ch = 'D';
+	key.at(3).number = 3;
+	key.at(3).ch = 'P';
+	key.at(4).number = 4;
+	key.at(4).ch = 'B';
 
 
 
@@ -40,6 +58,12 @@ int main()
 	for (int i = 0; i < myV.size(); i++) {
 		cout << myV[i] << endl;
 	}
+	/*for (int i = 0; i < key.size(); i++) {
+		cout << key.at(i).ch << "	";
+		cout << key.at(i).number << endl;
+	}*/
+	//decryption(myV, key);
+
 	cout << endl;
 	cout << endl;
 	encryption(myV, key);
@@ -48,13 +72,12 @@ int main()
 	}
 	cout << endl;
 	cout << endl;
+
 	decryption(myV, key);
 	for (int i = 0; i < myV.size(); i++) {
 		cout << myV[i] << endl;
 	}
-	//p(key, key.size());
-	//
-	//p(key, key.size());
+	
 }
 
 
@@ -151,11 +174,12 @@ void input(vector<T>& arr,const int sizeKey)
 }
 
 template<class T, class B>
-void encryption(vector<T>& arr, vector<B> key)
+void encryption(vector<T>& arr, vector<B>& key)
 {
 	vector<B> BUFF;
 	BUFF = key;
 	B tmp;
+	char tmp_1;
 	string b;
 	for (int k = 0; k < arr.size(); k++)
 	{
@@ -164,24 +188,25 @@ void encryption(vector<T>& arr, vector<B> key)
 		for (int i = 0; i < b.size(); i++) // i - номер текущего шага
 		{
 			int pos = i;
-			tmp =  b[i];
+			tmp_1 =  b[i];
 			for (int j = i + 1; j < b.size(); j++) // цикл выбора наименьшего элемента
 			{
 	
-				if (key[j] < key[i])
+				if (key.at(j).ch < key.at(i).ch)
 				{
-					tmp = b[j];
+					tmp_1 = b[j];
 					b[j] = b[i];
-					b[i] = tmp;
+					b[i] = tmp_1;
 					
-					tmp = key[j];
-					key[j] = key[i];
-					key[i] = tmp;
+					tmp = key.at(j);
+					key.at(j) = key.at(i);
+					key.at(i) = tmp;
 				}
 
 			}
 			// меняем местами наименьший с a[i]
 		}
+
 		arr[k] = b;
 	}
 
@@ -193,8 +218,9 @@ void decryption(vector<T>& arr, vector<B> key)
 {
 	vector<B> BUFF;
 	BUFF = key;
-	selectSort(key, key.size());
+	//selectSort(key, key.size());
 	B tmp;
+	char tmp_1;
 	string b;
 	for (int k = 0; k < arr.size(); k++)
 	{
@@ -203,24 +229,25 @@ void decryption(vector<T>& arr, vector<B> key)
 		for (int i = 0; i < b.size(); i++) // i - номер текущего шага
 		{
 			int pos = i;
-			tmp = b[i];
+			tmp_1 = b[i];
 			for (int j = i + 1; j < b.size(); j++) // цикл выбора наименьшего элемента
 			{
 
-				if (key[j] < BUFF[i])
+				if (key.at(j).number < key.at(i).number)
 				{
-					tmp = b[j];
+					tmp_1 = b[j];
 					b[j] = b[i];
-					b[i] = tmp;
+					b[i] = tmp_1;
 
-					tmp = key[j];
-					key[j] = key[i];
-					key[i] = tmp;
+					tmp = key.at(j);
+					key.at(j) = key.at(i);
+					key.at(i) = tmp;
 				}
 
 			}
 			// меняем местами наименьший с a[i]
 		}
+
 		arr[k] = b;
 	}
 
