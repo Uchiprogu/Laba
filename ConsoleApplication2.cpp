fcad938c65c68533113911e9b2d0e43414672b19
uchiprogu@uchiprogu-T100TA <you@example.com>
2019-05-05 09:44:49 +0300

    Victor

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -7,8 +7,6 @@
 #include <cctype> //tolower
 #include <string>
 
-#include <stdio.h>
-#include <ctype.h>
 
 using namespace std;
 
@@ -20,31 +18,100 @@ void p(const vector<T> &arr, int size = 0);
 template< class T >
 void input(vector<T> &arr,const int sizeKey = 0);
 
+void print(const string& str);
+
 template< class T, class B>
-void encryption(vector<T> &arr, vector<B> key);
+void encryption(vector<T> &arr, vector<B>& key);
 template< class T, class B>
 void decryption(vector<T> &arr, vector<B> key);
 
+struct Key
+{
+	char ch;
+	int number;
+};
+
 int main()
 {
-	vector<char> key;
+	Key keyCharacter;
+	vector<Key> key;
 	vector<string> myV;
+
+	string keyStr;
+	getline(cin, keyStr);
 	char ch;
-	while (cin.get(ch)){
-		if (!isalpha(ch))
-			break;
-		if(ch > 96)
-			ch = ch - 32;
-		key.push_back(ch);
+	int k =1;
+	for(int i =0; i< keyStr.size(); i++)
+	{
+		ch = keyStr[i];
+		//cout<< ch <<" ";
+
+		for(int j =k; j< keyStr.size(); j++)
+		{
+			if(keyStr[j] == ch)
+			{
+				cout<<"Input key without similar chars"<<endl;
+				exit(0);
+			}
+		}
+		k++;
+
 	}
-	p(key, key.size());
+
+	cout<< endl; 
+	for(int i =0; i< keyStr.size(); i++)
+	{
+		key.push_back(keyCharacter);
+		key.at(i).number = i;
+		key.at(i).ch = keyStr[i];
+	}
+	
+	cout << "-----------" << endl;
+	for (int i = 0; i < key.size(); i++)
+	{
+		cout << key.at(i).ch;
+		cout << " ";
+		//cout << key.at(i).number;
+	}
+	cout << endl;
+	cout << "-----------" << endl; 
+
+
+	/*key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+	key.push_back(keyCharacter);
+
+	key.at(0).number = 0;
+	key.at(0).ch = 'T';
+	key.at(1).number = 1;
+	key.at(1).ch = 'E';
+	key.at(2).number = 2;
+	key.at(2).ch = 'X';
+	key.at(3).number = 3;
+	key.at(3).ch = 'T';
+	key.at(4).number = 4;
+	key.at(4).ch = 'B';*/
 
 
 
 	input(myV, key.size());
+	if(keyStr.size() > myV.size())
+	{
+				cout<<"Input key < str"<<endl;
+				exit(0);
+	}
 	for (int i = 0; i < myV.size(); i++) {
 		cout << myV[i] << endl;
 	}
+
+	/*for (int i = 0; i < key.size(); i++) {
+		cout << key.at(i).ch << "	";
+		cout << key.at(i).number << endl;
+	}*/
+	//decryption(myV, key);
+
 	cout << endl;
 	cout << endl;
 	encryption(myV, key);
@@ -53,13 +120,12 @@ int main()
 	}
 	cout << endl;
 	cout << endl;
+
 	decryption(myV, key);
 	for (int i = 0; i < myV.size(); i++) {
 		cout << myV[i] << endl;
 	}
-	//p(key, key.size());
-	//
-	//p(key, key.size());
+	
 }
 
 
@@ -115,7 +181,8 @@ void input(vector<T>& arr,const int sizeKey)
 	// I'm confused about whether you want a line, or a word.
 	// this gets a line
 	//std::getline(std::cin, str);
-	str = ("No description, website, or topics provided.");
+	str = ("in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+	print(str);
 	// this gets a word
 	//std::cin >> str;
 
@@ -156,11 +223,12 @@ void input(vector<T>& arr,const int sizeKey)
 }
 
 template<class T, class B>
-void encryption(vector<T>& arr, vector<B> key)
+void encryption(vector<T>& arr, vector<B>& key)
 {
 	vector<B> BUFF;
 	BUFF = key;
 	B tmp;
+	char tmp_1;
 	string b;
 	for (int k = 0; k < arr.size(); k++)
 	{
@@ -169,24 +237,25 @@ void encryption(vector<T>& arr, vector<B> key)
 		for (int i = 0; i < b.size(); i++) // i - номер текущего шага
 		{
 			int pos = i;
-			tmp =  b[i];
+			tmp_1 =  b[i];
 			for (int j = i + 1; j < b.size(); j++) // цикл выбора наименьшего элемента
 			{
 	
-				if (key[j] < key[i])
+				if (key.at(j).ch < key.at(i).ch)
 				{
-					tmp = b[j];
+					tmp_1 = b[j];
 					b[j] = b[i];
-					b[i] = tmp;
+					b[i] = tmp_1;
 					
-					tmp = key[j];
-					key[j] = key[i];
-					key[i] = tmp;
+					tmp = key.at(j);
+					key.at(j) = key.at(i);
+					key.at(i) = tmp;
 				}
 
 			}
 			// меняем местами наименьший с a[i]
 		}
+
 		arr[k] = b;
 	}
 
@@ -198,46 +267,50 @@ void decryption(vector<T>& arr, vector<B> key)
 {
 	vector<B> BUFF;
 	BUFF = key;
-	selectSort(key, key.size());
+	//selectSort(key, key.size());
 	B tmp;
+	char tmp_1;
 	string b;
 	for (int k = 0; k < arr.size(); k++)
 	{
 		b = arr[k];
 		key = BUFF;
-		for (int i = 0; i < b.size(); i++) 
+		for (int i = 0; i < b.size(); i++) // i - номер текущего шага
 		{
 			int pos = i;
-			tmp = b[i];
-			for (int j = i + 1; j < b.size(); j++) 
+			tmp_1 = b[i];
+			for (int j = i + 1; j < b.size(); j++) // цикл выбора наименьшего элемента
 			{
 
-				if (key[j] < BUFF[i])
+				if (key.at(j).number < key.at(i).number)
 				{
-						tmp = b[j];
-						b[j] = b[i];
-						b[i] = tmp;
+					tmp_1 = b[j];
+					b[j] = b[i];
+					b[i] = tmp_1;
 
-					tmp = key[j];
-					key[j] = key[i];
-					key[i] = tmp;
+					tmp = key.at(j);
+					key.at(j) = key.at(i);
+					key.at(i) = tmp;
 				}
 
 			}
-			
+			// меняем местами наименьший с a[i]
 		}
-	/*	if(k = arr.size() - 1)
-		{
-			for(int i = b.size() - 1; i > -1; i--)
-			{
-				if(b[i] == '*')
-					b.erase(i);
-			}
-			arr[k] = b;
-		}else*/
-			arr[k] = b;
+
+		arr[k] = b;
 	}
 
 }
 
 
+void print(const string& str)
+{
+	cout << "-----------" << endl;
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str[i];
+	}
+	cout << endl;
+	cout << "-----------" << endl;
+
+}
